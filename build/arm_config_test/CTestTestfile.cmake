01dd04573664941841f24236d5c67feed2279910
Huangjk <442827947@qq.com>
2025-02-24 21:51:55 +0800

    moveit2可用

---
+++ build/arm_config_test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/huang/chapt8_ws/src/arm_config_test
+# Build directory: /home/huang/chapt8_ws/build/arm_config_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/arm_config_test/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/huang/chapt8_ws/install/fishbot_description/share/fishbot_description/package.sh"
